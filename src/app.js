cc2532897b0211df01b6575e46fe6ec87cada456
architi <aryaamightbeclueless@gmail.com>
2026-03-07 19:14:19 +0530

    playin around with routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 const express = require("express");
-
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("this is req handler for test route");
+app.use("/home", (req, res) => {
+  res.send("welcome this is the home page");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("hello from 7777 port");
- });
+app.get("/user", (req, res) => {
+  res.send({ firstName: "architi", lastName: "dadhwal" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("omg it looks like the db has saved the data sucessfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("data updated successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("you rmbr the data? yeah its deleted now");
+});
 
 app.listen(7777, () => {
-  console.log("welcome, 7777 web server is listening!");
+  console.log("hello user!the server is listening on port 7777");
 });
