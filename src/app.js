753feca646bd328aef4b7ef5d8908f143579582e
architi <aryaamightbeclueless@gmail.com>
2026-03-09 01:05:03 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const app = express();
 const {adminAuth, userAuth } = require("./middlewares/Auth.jsx")
 
-app.use("/route", adminAuth);
-
-app.get("/route/One", (req,res)=>{
-    res.send("user data sent");
+//way 1 specifically look out for this route but this is a good way to do it 
+app.get("/route",(err,req,res,next)=>{
+    try {
+        //logic
+        throw new Error("this is an error");
+    } catch (error) {
+        res.status(500).send("something went wrong , contact support team");
+    }
 })
 
-app.delete("/user/data",userAuth, (req,res) => {
-    res.send("user data has been deleted!");
-})
+//way 2 this is a universal way this handles all errors in the app as the route is "/" and uses the "use" method
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("something went wrong , contact support team");
+    }
+});
 
-app.use("/user/login", (req,res)=>{
-    res.send("hi there!")
-})
 
 app.listen(7777,()=>{
     console.log("Server is listening / running on port 7777");
