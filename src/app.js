6b4f9925e1df79dcc9d5e8ccfca435f35ac54429
architi <aryaamightbeclueless@gmail.com>
2026-03-09 23:21:16 +0530

    connecting mongoose db

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 const express = require("express");
+const ConnectDB = require("./config/database");
 const app = express();
-const {adminAuth, userAuth } = require("./middlewares/Auth.jsx")
 
-//way 1 specifically look out for this route but this is a good way to do it 
-app.get("/route",(err,req,res,next)=>{
-    try {
-        //logic
-        throw new Error("this is an error");
-    } catch (error) {
-        res.status(500).send("something went wrong , contact support team");
-    }
-})
-
-//way 2 this is a universal way this handles all errors in the app as the route is "/" and uses the "use" method
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("something went wrong , contact support team");
-    }
-});
+ConnectDB()
+    .then(()=>{
+        console.log("now the db is connect and only THEN the server is listening");
+        app.listen(7777,()=>{
+            console.log("Server is listening / running on port 7777");
+        });
+    })
+    .catch((error)=>{
+    console.log("caught an error mb dawg")
+    });
 
 
-app.listen(7777,()=>{
-    console.log("Server is listening / running on port 7777");
-})
