407e3886fb8fe0ad3a3e85dbc38f46e7928509d7
architi <aryaamightbeclueless@gmail.com>
2026-03-12 23:26:53 +0530

    setting up the db connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
-const ConnectDB = async()=>{
-    await mongoose.connect("mongodb+srv://dbUser:4E7m5Ko9m5Alk9Gr@clustermuster.lkwjbna.mongodb.net/devTinder")
-}
+const ConnectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://dbUser:4E7m5Ko9m5Alk9Gr@clustermuster.lkwjbna.mongodb.net/devTinder",
+  );
+};
 
-exports.module ={ ConnectDB};
+module.exports = ConnectDB;
