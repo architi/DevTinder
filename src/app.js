335ae1341479f437cb4634126108e27387bc3651
architi <aryaamightbeclueless@gmail.com>
2026-03-17 18:47:36 +0530

    crud API

--- src/app.js
+++ src/app.js
@@ -7,12 +7,48 @@ app.use(express.json());
 
 app.get("/feed", async (req, res) => {
   try {
-    const feed = User.find({});
+    const feed = await User.find({});
     res.send(feed);
   } catch (error) {
     res.status(400).send("error saving the user" + error.message);
   }
 });
+
+app.get("/user", async (req,res) => {
+    const UserEmailId = req.body.emailId;
+
+    try {
+        const users = await User.find({ emailId : UserEmailId });
+        res.send(users)
+
+    } catch (error) {
+        res.send("error has been detected in fetching user by emailId")
+    }
+})
+
+app.delete("/user",async(req,res) => {
+   const userId = req.body.userId;
+   try {
+    const del = await User.findByIdAndDelete(userId);
+    //const del = await User.findByIdAndDelete({_id: userId});
+
+    res.send("deleted this successfully",del);
+   } catch (err) {
+    res.status(400).send("caught some error")
+   }
+})
+
+//API - to update data of the user 
+app.patch("/user",async(req,res)=>{
+    const userIdToUpdate = req.body.userId;
+    const updatedData = req.body;
+
+    try {
+         await User.findOneAndUpdate({_id:userIdToUpdate},updatedData);   
+    } catch (error) {
+        res.status(400).send("something went wrong while updating")
+    }
+})
 
 ConnectDB()
   .then(() => {
