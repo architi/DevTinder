fbfe9149ab9ba6f62c9213b700f384a4e4dcd644
architi <aryaamightbeclueless@gmail.com>
2026-03-26 01:45:31 +0530

    data sanitizing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const app = express();
 const ConnectDB = require("./config/database");
 const User = require("./models/user");
-const app = express();
 
 app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(400).res("error in signing up please check" + error.message);
+  }
+});
 
 app.get("/feed", async (req, res) => {
   try {
@@ -14,41 +21,57 @@ app.get("/feed", async (req, res) => {
   }
 });
 
-app.get("/user", async (req,res) => {
-    const UserEmailId = req.body.emailId;
+app.get("/user", async (req, res) => {
+  const UserEmailId = req.body.emailId;
 
-    try {
-        const users = await User.find({ emailId : UserEmailId });
-        res.send(users)
+  try {
+    const users = await User.find({ emailId: UserEmailId });
+    res.send(users);
+  } catch (error) {
+    res.send("error has been detected in fetching user by emailId");
+  }
+});
 
-    } catch (error) {
-        res.send("error has been detected in fetching user by emailId")
-    }
-})
-
-app.delete("/user",async(req,res) => {
-   const userId = req.body.userId;
-   try {
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
     const del = await User.findByIdAndDelete(userId);
     //const del = await User.findByIdAndDelete({_id: userId});
 
-    res.send("deleted this successfully",del);
-   } catch (err) {
-    res.status(400).send("caught some error")
-   }
-})
+    res.send("deleted this successfully", del);
+  } catch (err) {
+    res.status(400).send("caught some error");
+  }
+});
 
-//API - to update data of the user 
-app.patch("/user",async(req,res)=>{
-    const userIdToUpdate = req.body.userId;
-    const updatedData = req.body;
+//API - to update data of the user
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
 
-    try {
-         await User.findOneAndUpdate({_id:userIdToUpdate},updatedData);   
-    } catch (error) {
-        res.status(400).send("something went wrong while updating")
+  try {
+    const updationAllowed = ["skills", "gender"];
+    const checkUpdation = object
+      .keys(data)
+      .every((key) => updationAllowed.includes(key));
+
+    if (!checkUpdation) {
+      throw new error("update not allowed");
     }
-})
+
+    if (data?.skills.length>10) {
+      throw new error("skills cant be more than 10")
+    }
+    const user = await User.findOneAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("user updated successfully!");
+  } catch (error) {
+    res.status(400).send("something went wrong while updating");
+  }
+});
 
 ConnectDB()
   .then(() => {
