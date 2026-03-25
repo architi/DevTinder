23c9e1bb4b52bc25ee4d68099ee2486932f8283e
architi <aryaamightbeclueless@gmail.com>
2026-03-26 01:46:20 +0530

    playing around schema types

--- src/models/user.js
+++ src/models/user.js
@@ -4,16 +4,47 @@ const Mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    require: true,
+    minLength: 2,
+    maxLength: 20,
   },
   lastName: {
     type: String,
+    minLength: 2,
+    maxLength: 20,
   },
   emailId: {
     type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 20,
+    trim: true,
+    unique: true,
+    lowercase: true,
   },
   password: {
     type: String,
+    minLength: 2,
+    maxLength: 20,
   },
+  skills: {
+    type: [String],
+  },
+  phoneNumber: {
+    type: Number,
+    default: "XXX-XXXX-XXX",
+    min: 10,
+    max: 10,
+  },
+  gender:{
+    type: String,
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new error ("gender data is not valid")
+      }
+    }
+  },
+  timestamp: true,
 });
 
 const User = Mongoose.model("User", userSchema);
