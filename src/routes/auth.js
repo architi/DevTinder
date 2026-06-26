a08377bcdcb723093665cc0a1d0357ac7c7c06e3
architi <aryaamightbeclueless@gmail.com>
2026-06-26 23:54:34 +0530

    fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,35 @@ const authRouter = express.Router();
 const { validateSignUp } = require("../utils/validatation");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+
+//login api
+/*** 
+ //this is where compare happens cause we want to check the password and this is where jwt is created and res is sent via cookie
+  
+ {emailId, password} = req.body
+ const emailValid = User.findOne({emailId:emailId});
+
+ await bcrypt.compare(password,passwordHash);
+ //create jwt if the password correct too
+ const token = await jwt.sign({_id:user_id},"key");
+
+ sending it off
+ res.cookie ("token",token);
+
+ //profileedit api accesiing the cookie
+ app.patch("/profile", async(res,req)=>{
+    const cookie = req.cookies;
+    //destructing the token out 
+    {token} = cookie;
+    //veryfing that token
+    const myToken = await jwt.verify(token,"key");
+    const {_id} = myToken
+  })
+
+
+*/
 
 authRouter.post("/signup", async (req, res) => {
   //creating new instance in the database-collection
@@ -17,7 +45,7 @@ authRouter.post("/signup", async (req, res) => {
     //extract fields
     const { firstName, lastName, emailId, password } = req.body;
 
-    //password encryption
+    //password encryption by hashing
     const passwordHash = await bcrypt.hash(password, 10);
 
     
@@ -36,24 +64,28 @@ authRouter.post("/signup", async (req, res) => {
   }
 });
 
+//jwt token is made during login
 authRouter.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
 
-    const validUser = await User.findOne({ emailId: emailId });
-    if (!validUser) {
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
       throw new Error("invalid credentials");
     }
 
-    const validPassword = await validUser.validatePassword(password);
+    //bcyprt.compare is happening in user schema its a method now called validatePassword
+    const validPassword = await user.validatePassword(password);
+
     if (validPassword) {
       //creating a JWT token
-      const token = await validUser.getJWT();
+      //jwt.sign is sent off to userSchema its now a method
+      const token = await user.getJWT();
 
       res.cookie("token", token, {
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       });
-      res.send("successful login!");
+      res.send("login successfully!");
     } else {
       throw new Error("invalid credentials");
     }
