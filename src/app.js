14add4493413383eef708ce36c991e4941acb8d3
architi <aryaamightbeclueless@gmail.com>
2026-07-02 23:44:34 +0530

    cleanin

--- src/app.js
+++ src/app.js
@@ -8,8 +8,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
-const requestRouter = require("./routes/request");
 const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+
+
+app.use("/", authRouter);
+app.use("/", profileRouter); 
+app.use("/", requestRouter);
 
 /*** 
  upating the data present in database
