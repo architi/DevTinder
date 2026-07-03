8fe5d4493ac8b55edb6f7961c72ddeb49b41b167
architi <aryaamightbeclueless@gmail.com>
2026-07-03 23:53:56 +0530

    cleaned

--- src/app.js
+++ src/app.js
@@ -16,56 +16,6 @@ app.use("/", authRouter);
 app.use("/", profileRouter); 
 app.use("/", requestRouter);
 
-/*** 
- upating the data present in database
-
-app.patch("/user", async(req,res)=>{
-  const userId = req.body.userId;
-  const data = req.body;
-  const finding = User.findByIdAndUpdate(userId, data, { runValidators: true }) 
-  })
-
-  api level validation
-app.patch("/user", async(req,res)=>{
-  const userData = req.body;
-  const allowedFieldsForUpdate = ["name", "skills"];
-  const canIUpdate = Object.keys(userData).every((k) => allowedFieldsForUpdate.includes(k));
-  if(!canIUpdate){
-  throw new error("sorry smth went wrong")}
-  })
-
-  signup api
-  app.post("/signup", async(req,res)=>{
-
-    //validation of our signup req.body
-    const validationSignUp = (req)=>{
-      //destructer using js
-      const {firstName,lastName,password,emailId} = req.body;
-
-      if(!firstName || !lastName){
-      throw new Error("please fill the fields")}
-      elseif(!validator.isEmailId(emailId)){
-      throw new error("please fill the emailId correctly")}
-      }
-
-      //encrypt the password
-      
-      //destructure the fields
-      const {emailId, password, firstName, lastName} = req.body;
-
-      //encrypt the password
-      const passwordHash = await bcrypt(password,10);
-
-      //save the user model instance and User is the class from our schema
-      const user = new User ({
-        firstName,
-        lastName,
-        password:passwordHash,
-        emailId
-      })
-        await user.save();
-      })
-***/
 
 ConnectDB()
   .then(() => {
