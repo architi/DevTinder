b64683587ff1f06f17b94d83a6bdcf09ff521fe0
architi <aryaamightbeclueless@gmail.com>
2026-09-17 21:41:29 +0530

    minor dets

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const requestRouter = express.Router();
-const { userAuth } = require("../middlewares/Auth");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
+const { userAuth } = require("../middlewares/Auth");
+const ConnectionRequest = require("../models/connectionRequest");
+
+//dynamic for interested & ignored
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
   try {
-    console.log("connection request sent");
-    res.send("connection request sent");
+    const fromUserId = req.user._id;
+    const status = req.params.status;
+    const toUserId = req.params.toUserId;
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+
+    const data = await connectionRequest.save();
+
+    res.json({
+      message:"connection request sent successfully",
+      data,
+    })
+
   } catch (error) {
-    res.status(400).send("error in sending connection request");
+    res.status(400).send("ERROR:"+ error.message);
   }
+
+  res.send(req.user.firstName + "sent the connection request");
 });
 
 module.exports = requestRouter;
