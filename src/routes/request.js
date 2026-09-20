0aa60b9e7d7097e3708658a9f1d9bf1c23191405
architi <aryaamightbeclueless@gmail.com>
2026-09-21 03:30:32 +0530

    some validations

--- src/routes/request.js
+++ src/routes/request.js
@@ -11,6 +11,32 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     const status = req.params.status;
     const toUserId = req.params.toUserId;
 
+    //checking if the status is valid
+    const allowedFields = ["interested","ignored"];
+    if(!allowedFields.includes(status)){
+      return res
+      .status(400)
+      .json({ message:"invalid status type:"+ status});
+    }
+
+    //checking for duplicate request
+//   $or: [
+//   { fromUserId: A, toUserId: B },   // Akshay → Architi
+//   { fromUserId: B, toUserId: A },   // Architi → Akshay
+// ]
+    const existingRequest = await ConnectionRequest.findOne({
+      $or:[
+        {fromUserId,toUserId},
+        {fromUserId:toUserId, toUserId:fromUserId}
+      ]
+    });
+
+    if(existingRequest){
+      return res.status(400).json({
+        message: "request already sent to this user, please wait for their response"
+      })
+    }
+
     const connectionRequest = new ConnectionRequest({
       fromUserId,
       toUserId,
@@ -27,8 +53,8 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
   } catch (error) {
     res.status(400).send("ERROR:"+ error.message);
   }
-
-  res.send(req.user.firstName + "sent the connection request");
 });
 
 module.exports = requestRouter;
+
+
