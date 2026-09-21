ec317a37e4576084c5fd00966d4b38c052a58232
architi <aryaamightbeclueless@gmail.com>
2026-09-22 00:25:27 +0530

    add-toUser exist check

--- src/routes/request.js
+++ src/routes/request.js
@@ -4,13 +4,16 @@ const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/Auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
-//dynamic for interested & ignored
+const User = require("../models/user");
+
+//dynamic for interested & ignored only
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
   try {
     const fromUserId = req.user._id;
     const status = req.params.status;
     const toUserId = req.params.toUserId;
 
+//CHECK1
     //checking if the status is valid
     const allowedFields = ["interested","ignored"];
     if(!allowedFields.includes(status)){
@@ -19,6 +22,7 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       .json({ message:"invalid status type:"+ status});
     }
 
+//CHECK2
     //checking for duplicate request
 //   $or: [
 //   { fromUserId: A, toUserId: B },   // Akshay → Architi
@@ -30,12 +34,24 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
         {fromUserId:toUserId, toUserId:fromUserId}
       ]
     });
-
     if(existingRequest){
       return res.status(400).json({
         message: "request already sent to this user, please wait for their response"
       })
     }
+
+//CHECK3
+    //checking for toUserId exists in the db 
+    const toUser = await User.findById(toUserId);
+    if(!toUser){
+      return res.status(404).json({
+        message:"user with this id does not exist"
+      })
+    }
+
+//CHECK4
+ //checking if the user is trying to send request to themselves
+ 
 
     const connectionRequest = new ConnectionRequest({
       fromUserId,
